657d00874cf64602302248303e607c33f7f36ccb
Fahmi Idris <fahmi@Fahmi-Idris.local>
2022-09-28 14:53:54 +0700

    fix: debug remote entry api

--- pages/api/remote-entry.js
+++ pages/api/remote-entry.js
@@ -6,9 +6,13 @@ const WHITELISTED = process.env.WHITELISTED_DOMAIN;
 const handler = api().get(async (req, res) => {
   // if (req.headers.referer === WHITELISTED) {
     const isProduction = process.env.NODE_ENV === 'production';
+    console.log(isProduction);
     const remoteEntryPath = isProduction ? '_next/static/runtime/remoteEntry.js' : '.next/static/runtime/remoteEntry.js'
+    console.log(remoteEntryPath);
     const filePath = path.join(process.cwd(), remoteEntryPath);
+    console.log(filePath);
     const file = fs.readFileSync(filePath, { encoding: 'utf-8' });
+    console.log(file)
     return res.send(file)
   // }
   res.statusCode = 200
