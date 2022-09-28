bbc7378db587930aeabea1a64a604d23e30d23c9
Fahmi Idris <fahmi@Fahmi-Idris.local>
2022-09-28 14:50:34 +0700

    fix: debug remote entry api

--- pages/api/remote-entry.js
+++ pages/api/remote-entry.js
@@ -4,13 +4,13 @@ import { api } from '../../utils/api';
 
 const WHITELISTED = process.env.WHITELISTED_DOMAIN;
 const handler = api().get(async (req, res) => {
-  if(req.headers.referer === WHITELISTED) {
+  // if (req.headers.referer === WHITELISTED) {
     const isProduction = process.env.NODE_ENV === 'production';
     const remoteEntryPath = isProduction ? '_next/static/runtime/remoteEntry.js' : '.next/static/runtime/remoteEntry.js'
     const filePath = path.join(process.cwd(), remoteEntryPath);
     const file = fs.readFileSync(filePath, { encoding: 'utf-8' });
     return res.send(file)
-  }
+  // }
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   return res.end(JSON.stringify({ status: 'error', message: 'not whitelisted', whitelisted: WHITELISTED }))
